23b71ca68e1cd3bf6cafdcb5a786fa3a410d5522
mostafa <mostafamohamed62005@gmail.com>
2024-10-28 12:18:05 +0300

    Finished the help Command

--- src/main/java/org/cli/Main.java
+++ src/main/java/org/cli/Main.java
@@ -2,14 +2,38 @@ package org.cli;
 import org.cli.commands.*;
 import org.cli.utils.pathresolvers.*;
 
+import javax.naming.spi.Resolver;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        PathResolver resolver = LinuxPathResolver.getInstance();
+        Command hlp = new helpCommand();
+        hlp.execute(new String[]{"hello"});
 
-        PathResolver resolver = PathResolverFactory.getResolver() ;
-        CommandsController d = new CommandsController(resolver);
-        d.Run();
+        /*
+        Stream<Path> files = Files.list(Paths.get("/home/omar/Documents/"));
+        files.forEach(file -> System.out.println(file.getFileName()));
+
+        * */
+
 
     }
 }
 
+/*
+* valid file to valid folder
+*
+* invalid file to valid folder
+* valid file to existing file
+* valid file to non existing
+*
+* */
